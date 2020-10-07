8cc6f45cee87bae53a889ad5395c561d7de8182f
LSMicah <55545353+LSMicah@users.noreply.github.com>
2020-10-07 17:14:38 +0800

    Update the drawer part

--- app/src/main/java/com/example/damien/realworldproject/customer.java
+++ app/src/main/java/com/example/damien/realworldproject/customer.java
@@ -1,16 +1,133 @@
 package com.example.damien.realworldproject;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class customer extends AppCompatActivity {
+
+    //Initialize drawer variables
+    DrawerLayout drawerLayout;
+    //
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer);
+
+        //drawer coding part
+        drawerLayout = findViewById(R.id.drawer_layout);
+
+        //
+
     }
+
+    //drawer coding part
+    public void ClickMenu(View view){
+        //open drawer
+        openDrawer(drawerLayout);
+
+    }
+
+    public static void openDrawer(DrawerLayout drawerLayout) {
+        //open drawer layout
+        drawerLayout.openDrawer(GravityCompat.START);
+
+    }
+
+    public void ClickLogo(View view){
+        //Close drawer
+        closeDrawer(drawerLayout);
+
+    }
+
+    public static void closeDrawer(DrawerLayout drawerLayout) {
+        //Close drawer layout
+        //check condition
+        if(drawerLayout.isDrawerOpen(GravityCompat.START)){
+            //When drawer is open
+            //CLose drawer
+            drawerLayout.closeDrawer(GravityCompat.START);
+
+        }
+    }
+
+    public void ClickHome(View view){
+        //Recreate activity
+        recreate();
+
+    }
+
+    public void ClickDashboard(View view){
+        //Redirect activity to dashboard
+        redirectActivity(this,Dashboard.class);
+
+    }
+
+    public void ClickAboutUs(View view){
+        //Redirect activity to about us
+        redirectActivity(this,AboutUs.class);
+
+    }
+
+    public void ClickLogout(View view){
+        //Close app
+        logout(this);
+    }
+
+    public static void logout(final Activity activity) {
+        //Initialize alert dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity);
+        //set title
+        builder.setTitle("Logout");
+        //set message
+        builder.setMessage("Confirm logout ?");
+        //positive yes button
+        builder.setPositiveButton("YES", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //Finish activity
+                activity.finishAffinity();
+                //Exit app
+                System.exit(0);
+            }
+        });
+        //negative no button
+        builder.setNegativeButton("NO", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                //Dismiss dialog
+                dialog.dismiss();
+            }
+        });
+        //show dialog
+        builder.show();
+    }
+
+    public static void redirectActivity(Activity activity, Class aClass) {
+        //Initialize intent
+        Intent i = new Intent(activity,aClass);
+        //Set flag
+        i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        //Start activity
+        activity.startActivity(i);
+
+    }
+
+    protected void onPause(){
+        super.onPause();
+        //Close drawer
+        closeDrawer(drawerLayout);
+
+    }
+
+    //
 
     @Override
     public void onBackPressed() {
@@ -19,4 +136,5 @@ public class customer extends AppCompatActivity {
         a.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
         startActivity(a);
     }
+
 }
